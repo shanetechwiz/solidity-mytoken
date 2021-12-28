88a4a6c088979319502036fd74f8507e7df55fd0
Oshane Spencer <oshane.spencer@foodxtech.com>
2021-12-27 23:00:17 -0500

    Added transfer on-behalf functionality

--- OGToken.sol
+++ OGToken.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.10;
 
 contract OGToken {
     mapping (address => uint256) private _balances;
+    mapping (address => mapping(address => uint256)) private _allownaces;
 
     string private _name;
     string private _symbol;
@@ -9,6 +10,7 @@ contract OGToken {
     uint256 private _totalSupply;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
 
     constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol, uint8 decimalUnits) public {
         _balances[msg.sender] = initialSupply;
@@ -46,6 +48,14 @@ contract OGToken {
     function setBalance(address account, uint256 balance) internal {
         _balances[account] = balance;
     }
+    
+    function allowance(address owner, address spender, uint256 amount) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function setAllowance(address owner, address spender, uint256 amount) internal {
+        _allowances[owner][spender] = amount;
+    }
 
     function transfer(address beneficiary, uint256 amount) public returns (bool) {
         require(beneficiary != address(0), "Beneficiary address cannot be zero");
@@ -55,6 +65,30 @@ contract OGToken {
         _balances[msg.sender] -= amount;
         _balances[beneficiary] += amount;
         emit Transfer(msg.sender, beneficiary, amount);
+
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool success) {
+        require(spender != address(0), "Spender address cannot be zero");
+
+        _allownaces[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+
+        return true;
+    }
+
+    function transferFrom(address sender, address beneficiary, uint256 amount) public returns (bool) {
+        require(sender != address(0), "Sender address cannot be zero");
+        require(beneficiary != address(0), "Beneficiary address cannot be zero");
+        require(amount <= _allowances[sender][msg.sender], "Allowance is not enough");
+        require(_balances[sender] >= amount, "Sender does not have enough tokens");
+        require(_balances[beneficiary] + amount > _balances[beneficiary], "Addition overflow");
+
+        _balances[sender] -= amount;
+        _allownaces[sender][msg.sender] -= amount;
+        _balances[beneficiary] += amount;
+        emit Transfer(sender, beneficiary, amount);
 
         return true;
     }
